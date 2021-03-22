299eede5d27192421b4a9dc6441dc81bfa1ae028
Sriragh Ravindran <sriraghravindran@gmail.com>
2021-03-22 22:07:50 +0530

    Add files via upload

---
+++ tip_calculator.js
@@ -0,0 +1,28 @@
+function calculateTip(){
+    var billAmt=document.getElementById('bill').value;
+    var serviceQual=document.getElementById('tipAmt').value;
+    var numOfPeople=document.getElementById('people').value;
+
+if(billAmt===''||serviceQual==0){
+    alert('Value enter kar bsdk');
+    return;
+}
+if(numOfPeople==""||numOfPeople==0)
+{
+    numOfPeople=1;
+    document.getElementById('each').style.display='none';
+}
+else{
+    document.getElementById('each').style.display="block";
+}
+
+var total=(billAmt*serviceQual)/numOfPeople;
+total=Math.round(total);
+document.getElementById('totalTip').style.display='block';
+document.getElementById('tip').innerHTML=total;
+}
+document.getElementById('totalTip').style.display='none';
+document.getElementById("each").style.display='none';
+document.getElementById('button').onclick=function(){
+    calculateTip();
+}
